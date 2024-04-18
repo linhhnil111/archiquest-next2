c3db37142dee9495d472ab3d86ee89525807d85a
Gwyllim Jahn <gwyllo@gmail.com>
2024-04-18 20:02:45 +1000

    Added basic ai stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
+import Game from "./Game";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        Hello World This is some change...
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <Game />
       </div>
     </main>
   );
